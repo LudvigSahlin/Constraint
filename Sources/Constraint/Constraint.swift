7a87a60f337f05f759e817c85668b7a55985c441
Ludvig Sahlin <ludvigsahlin@hotmail.com>
2021-07-20 08:09:04 +0200

    Add Constraint code

--- Sources/Constraint/Constraint.swift
+++ Sources/Constraint/Constraint.swift
@@ -1,6 +1,44 @@
-public struct Constraint {
-    public private(set) var text = "Hello, World!"
+import Foundation
+#if os(iOS)
+import UIKit
+#endif
 
-    public init() {
+public struct Constraint {
+    enum ConstraintType { case top, bottom, leading, trailing, centerX, centerY, height, width }
+
+    let type: ConstraintType
+    let value: CGFloat
+
+    static let top = Constraint(type: .top, value: 0)
+    static let bottom = Constraint(type: .bottom, value: 0)
+    static let leading = Constraint(type: .leading, value: 0)
+    static let trailing = Constraint(type: .trailing, value: 0)
+    static let centerX = Constraint(type: .centerX, value: 0)
+    static let centerY = Constraint(type: .centerY, value: 0)
+    static let height = Constraint(type: .height, value: 0)
+    static let width = Constraint(type: .width, value: 0)
+    static func top(_ value: CGFloat) -> Constraint { return Constraint(type: .top, value: value) }
+    static func bottom(_ value: CGFloat) -> Constraint { return Constraint(type: .bottom, value: value) }
+    static func leading(_ value: CGFloat) -> Constraint { return Constraint(type: .leading, value: value) }
+    static func trailing(_ value: CGFloat) -> Constraint { return Constraint(type: .trailing, value: value) }
+    static func centerX(_ value: CGFloat) -> Constraint { return Constraint(type: .centerX, value: value) }
+    static func centerY(_ value: CGFloat) -> Constraint { return Constraint(type: .centerY, value: value) }
+    static func height(_ value: CGFloat) -> Constraint { return Constraint(type: .height, value: value) }
+    static func width(_ value: CGFloat) -> Constraint { return Constraint(type: .width, value: value) }
+
+    #if os(iOS)
+    func getConstraint(from view: UIView, to view2: UIView) -> NSLayoutConstraint {
+        switch type {
+        case .top: return view.topAnchor.constraint(equalTo: view2.topAnchor, constant: value)
+        case .bottom: return view.bottomAnchor.constraint(equalTo: view2.bottomAnchor, constant: value)
+        case .leading: return view.leadingAnchor.constraint(equalTo: view2.leadingAnchor, constant: value)
+        case .trailing: return view.trailingAnchor.constraint(equalTo: view2.trailingAnchor, constant: value)
+        case .centerX: return view.centerXAnchor.constraint(equalTo: view2.centerXAnchor, constant: value)
+        case .centerY: return view.centerYAnchor.constraint(equalTo: view2.centerYAnchor, constant: value)
+        case .height: return view.heightAnchor.constraint(equalTo: view2.heightAnchor, constant: value)
+        case .width: return view.widthAnchor.constraint(equalTo: view2.widthAnchor, constant: value)
+        }
     }
+    #endif
+
 }
--- Tests/ConstraintTests/ConstraintTests.swift
+++ Tests/ConstraintTests/ConstraintTests.swift
@@ -6,6 +6,6 @@ final class ConstraintTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Constraint().text, "Hello, World!")
+//        XCTAssertEqual(Constraint().text, "Hello, World!")
     }
 }
